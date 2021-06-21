7abfee6c7d0fb8e243bce4d5c696ad28681190ab
NamLa <nam.darkstar@gmail.com>
2021-06-22 03:52:38 +0700

    updatenew

---
+++ WebDangKyMonHoc/Models/MonDaDangKy.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace WebDangKyMonHoc.Models
+{
+    public class MonDaDangKy
+    {
+        [DisplayName("Mã Lớp Học Phần")]
+        public int MaLHP { get; set; }
+        [DisplayName("Tên Giáo Viên")]
+        public string TenGV { get; set; }
+        [DisplayName("Tên Môn Học")]
+        public string TenMH { get; set; }
+        [DisplayName("Tiết Học")]
+        public string TietHoc { get; set; }
+        [DisplayName("Phòng Học")]
+        public string PhongHoc { get; set; }
+        [DisplayName("Thứ")]
+        public string Thu { get; set; }
+    }
+}
